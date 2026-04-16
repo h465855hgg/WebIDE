c86f7e63a959efa20f790cb4ab43096ed47e6735
rrzt <124551348+h465855hgg@users.noreply.github.com>
2026-04-17 07:55:48 +0800

    chore(version): 更新应用版本号
    - 将 versionCode 从 31 更新为 32
    - 将 versionName 从 0.3.1 更新为 0.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.web.webide"
         minSdk = 29
         targetSdk = 36
-        versionCode = 31
-        versionName = "0.3.1"
+        versionCode = 32
+        versionName = "0.3.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         ndk {
             //noinspection ChromeOsAbiSupport
